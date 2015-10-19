14ef28a94aff726658d808d183a5c293ad4d9dac
Allie <aliyah.bolden@f1006-25-34156.bmcc.cuny.edu>
2015-10-19 14:35:26 -0400

    added crap

--- 10:19:15/js/main.js
+++ 10:19:15/js/main.js
@@ -1,6 +1,16 @@
+
+
 function setup() {
     //create the Canvas
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);  
+}
+
+
+function draw(){
+    fill(70, 27, 126);
+    stroke(230, 187, 255);
+    strokeWeight(5);
+    var rectWidth = 450;
+    rect(windowWidth/2 - rectWidth/2, 50, 450, 650);
     
-    rect(30, 20, 55, 55);
 }
