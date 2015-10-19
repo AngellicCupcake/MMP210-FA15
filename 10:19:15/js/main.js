9dd69afeba43d69b5aa0b822cd1301b915710e59
Allie <aliyah.bolden@f1006-25-34156.bmcc.cuny.edu>
2015-10-19 16:03:30 -0400

    loop
    made a loop for windows & edited window width and height

--- 10:19:15/js/main.js
+++ 10:19:15/js/main.js
@@ -1,19 +1,38 @@
 function setup() {
     //create the Canvas
-    createCanvas(windowWidth, windowHeight);  
+    createCanvas(windowWidth, 3600);  
      
-    drawWindow(600, 100);
+   /*drawWindow(600, 100);
     drawWindow(350, 100);
     drawWindow(100, 100);
+   */ 
+    
+    var x = 0;
+    var y = 0;
+    
+    for(var i=0; i<84; i++){
+console.log(i);
+       // drawWindow(50*i, 50*i);
+        
+        drawWindow(x, y);
+        x += 180;
+        
+        if(x>windowWidth-150){
+            x = 0;
+            y += 300;
+        }
+            
+}
+    
 }
  
 
 function drawWindow(startX, startY){
-     fill(70, 27, 126);
+    fill(70, 27, 126);
     stroke(230, 187, 255);
     strokeWeight(5);
     var rectWidth = 150;
-    rect(startX, startY, 150, 250);
+    rect(startX, startY, 150, 280);
     
     
 }
