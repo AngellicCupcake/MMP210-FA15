c5ba2453a627cfc0a220ab55772d0d20cf630a1a
Allie <aliyah.bolden@f1006-25-34156.bmcc.cuny.edu>
2015-10-19 14:54:31 -0400

    function
    added drawWindow and commented out draw function

--- 10:19:15/js/main.js
+++ 10:19:15/js/main.js
@@ -3,14 +3,28 @@
 function setup() {
     //create the Canvas
     createCanvas(windowWidth, windowHeight);  
+    
+    drawWindow();
 }
 
 
+/*
+// One Way to Draw It
 function draw(){
     fill(70, 27, 126);
     stroke(230, 187, 255);
     strokeWeight(5);
-    var rectWidth = 450;
-    rect(windowWidth/2 - rectWidth/2, 50, 450, 650);
+    var rectWidth = 350;
+    rect(windowWidth/2 - rectWidth/2, 50, 350, 650);
     
+}*/
+
+
+function drawWindow(){
+     fill(70, 27, 126);
+    stroke(230, 187, 255);
+    strokeWeight(5);
+    var rectWidth = 350;
+    rect(windowWidth/2 - rectWidth/2, 50, 350, 650);
 }
+
