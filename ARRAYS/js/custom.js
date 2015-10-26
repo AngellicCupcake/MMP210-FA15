39eaff0f36b272f4830a0723f53252b31651c188
Allie <aliyah.bolden@f1006-25-34156.bmcc.cuny.edu>
2015-10-26 15:05:03 -0400

    created 2 for loops
    created a for loop to go through array each time and add 10, and made a
    loop to count 1200 apples.

--- ARRAYS/js/custom.js
+++ ARRAYS/js/custom.js
@@ -7,14 +7,29 @@ function setup(){
     var cnv = createCanvas(800,3000);
     cnv.parent("windows");
     
+    var myArray = []; // Create blank array
+    //Add things
+    
+    for(var i=0; i<1201; i++){
+        myArray[i] = "apple number " + i;
+    }
+    
+    // log out of the array
+    console.log(myArray);
+    
+    //Loop to go through every item on the array.
+    
+    for(var i=0; i<myArray.length; i++){
+        myArray[i]+=10;
+        console.log(myArray[i]);
+    }
     
     
-    var myArray = ["hello",3,"bye"];
-    console.log(myArray[0]);
+    
+   /* console.log(myArray[0]);
     console.log(myArray[1]);
     console.log(myArray[2]);
-    console.log(myArray);
-    console.log("myArray", myArray);
+    console.log(myArray);*/
     
     
 } // Setup End
