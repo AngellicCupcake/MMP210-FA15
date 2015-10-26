25f6b406109ee158eb30a9e039e73d7ce638f30e
Allie <aliyah.bolden@f1006-25-34156.bmcc.cuny.edu>
2015-10-26 15:23:15 -0400

    added ellipse
    Added circles in the loop (comments added).

--- ARRAYS/js/custom.js
+++ ARRAYS/js/custom.js
@@ -4,14 +4,18 @@
 function setup(){
     // Create Canvas
     
-    var cnv = createCanvas(800,3000);
+    var cnv = createCanvas(windowWidth,windowHeight);
     cnv.parent("windows");
     
     var myArray = []; // Create blank array
     //Add things
     
     for(var i=0; i<1201; i++){
-        myArray[i] = "apple number " + i;
+ //       myArray[i] = "apple number " + i;
+        
+        // This adds circles to the array
+        //but we won't be able to address individual circles later
+        myArray[i] = ellipse(i,i,10,10);
     }
     
     // log out of the array
@@ -19,7 +23,8 @@ function setup(){
     
     //Loop to go through every item on the array.
     
-    for(var i=0; i<myArray.length; i++){
+    for(var i=0; i < myArray.length; i++){
+        // log out each item, for ellipses it will return the whole p5 object instead.
         myArray[i]+=10;
         console.log(myArray[i]);
     }
