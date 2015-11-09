d58b59749db74f3cfca7d0456d4e203815af21a6
Allie <aliyah.bolden@f1006-25-34156.bmcc.cuny.edu>
2015-11-09 16:12:04 -0500

    ADDED ALL THE BUNNIES

--- MIDTERM/js/main.js
+++ MIDTERM/js/main.js
@@ -7,31 +7,71 @@ function setup(){
     
     var cnv = createCanvas(windowWidth,windowHeight);
     cnv.parent("farm");
+    drawBunny();
+    var x = 100;
+    var y = 120;
+    for(var i=0; i<=40; i++){
+        //console.log(i);
+        drawBunny(x,y);
+        x += 180;
+        if( x > width){
+            x = 100;
+            y += 200;
+        }
+            
+    }
 }
 
 // Create Functions
 
-    function drawCat(meowX, meowY){
-        triangle(30, 75, 58, 20, 86, 75);
-    }
-
     function drawBunny(hopX, hopY){
+    console.log("hopX",hopX);   
+    earX = hopX-30;//70;
+    earY = hopY-70//50;
+    circleSize = 100;
         
-    }
-// Variables
-    startX = 0;
-    startY = 0;
-// Create Loop
+    earWidth = 25;
+    earLength = 75;
+    
+    bodyX = hopX;//100;
+    bodyY = hopY;//120;
+    
+    noseX = hopX-8;//92; //x
+    noseY = hopY+25;//145; //y
+    noseB = hopX;//100;// x
+    noseBB = hopY+30;//150; //y
+    noseD = hopX+8;//108; //x
+    noseDD = hopY+25;//145; //y
+        
+    pink = color(251, 187, 185 );
+    purple = color(218, 183, 255);
+    brown = color(111, 78, 55);
+    lineColor = red;
+        
+    eyeX = hopX-20;
+    eyeY = hopY;
+    eyeSize = 10;
+    
+    //draw the circle
+     
+    fill(pink);
+    ellipse(earX, earY, earWidth, earLength);
 
-    for(i=0; i<40; i++){
-        drawCat(20, 20);
-        drawCat(60, 20);
-        drawBunny(100, 20);
-        drawBunny(149, 20);
+    fill(pink);
+    ellipse(earX+60, earY, earWidth, earLength);
+    
+    fill(pink);
+    ellipse(bodyX, bodyY, circleSize, circleSize);
         
-        if(startX > windowWidth){
-            startX = 0;
-            startY += 100;
-        }
+    fill(brown);
+    triangle(noseX, noseY, noseB, noseBB, noseD, noseDD);
+    
+    fill(brown);
+    ellipse(eyeX, eyeY, eyeSize, eyeSize);
+        
+    fill(brown);
+    ellipse(eyeX+39, eyeY, eyeSize, eyeSize);
+    
+       
         
     }
