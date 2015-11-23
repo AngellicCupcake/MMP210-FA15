48dc79b83f3a1b903cfb1ff8bc1b71a1224f01fa
Allie <aliyah.bolden@f1006-25-34156.bmcc.cuny.edu>
2015-11-23 14:18:21 -0500

    added files
    do them later

---
+++ FINAL/Week11/Part 1/js/main.js
@@ -0,0 +1,10 @@
+// Global Variables
+
+
+
+function preload(){
+var url = 'https://raw.githubusercontent.com/dariusk/corpora/master/data/animals/birds_antarctica.json';
+
+    console.log("test");
+
+}
