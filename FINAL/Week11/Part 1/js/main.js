44854e4e56b99b41d78f3c125924de27809bcbab
Allie <aliyah.bolden@f1006-25-34156.bmcc.cuny.edu>
2015-11-23 15:12:55 -0500

    added canvas to part 1
    added canvas to part 1 and added notes

--- FINAL/Week11/Part 1/js/main.js
+++ FINAL/Week11/Part 1/js/main.js
@@ -1,22 +1,33 @@
 function setup(){
+    // Drew Canvas
+    var cnv = createCanvas(500, 500);
+    
     noLoop();
+    // Added JSON File
     var url = 'https://raw.githubusercontent.com/dariusk/corpora/master/data/animals/birds_antarctica.json' ;
+    // Loaded JSON File and called drawbird function
     loadJSON(url, drawBird);
     
     
 }
 
+// Colored canvas
 function draw(){
-    background(225,225,225);
+    background(225,225,225); // Gray
     
 }
 
+// Created drawBird Function
 function drawBird(birds){
+    // b is the arrays in one place
     b = birds.birds[5].members[1];
     loaded = true;
+    // Loads all the birds
     console.log(birds);
+    // Loads Specific penguin
     console.log(birds.birds[5].members[1]);
-    text(b, 0,0, 100, 100);
+    // Makes the Emperor Penguin visible on screen
+    text(b, 0, 0, 100, 100);
     
     
 }
