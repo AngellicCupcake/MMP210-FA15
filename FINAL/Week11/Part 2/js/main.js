8c722318eb3901d619aad443ab80e543b2f01c15
Allie <aliyah.bolden@f1006-25-34156.bmcc.cuny.edu>
2015-11-23 15:00:30 -0500

    second project done
    added the 3 birds in one array thing.

--- FINAL/Week11/Part 2/js/main.js
+++ FINAL/Week11/Part 2/js/main.js
@@ -1,3 +1,5 @@
+
+// Inserts the json file
 function setup(){
     noLoop();
     var url = 'https://raw.githubusercontent.com/dariusk/corpora/master/data/animals/birds_antarctica.json' ;
@@ -5,17 +7,24 @@ function setup(){
     
     
 }
-
+//Draws bg
 function draw(){
     background(225,225,225);
     
 }
 
+// draws birds
+// Makes text pop up
 function drawBird(birds){
-    var array = ["one, two, three"];
+    //To call in text
+    b = birds.birds[1].members;
     loaded = true;
     console.log(birds);
-    console.log(birds.birds[2].members[array]);
-
+    console.log(birds.birds[2].members[1]);
+    //Visible on page
     
+    for(var i=0; i < b.length; i++){
+    text(b, 0, 0, 100, 100);
+
+    }
 }
